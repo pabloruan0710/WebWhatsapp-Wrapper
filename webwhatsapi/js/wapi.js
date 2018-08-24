524e52443eb30f11b7d291548d0ba63e0eef4e90
Pablo Ruan - MacMini <pabloruan@outlook.com>
2018-08-24 16:38:51 -0300

    corrige erro envio msgs update whats

--- webwhatsapi/js/wapi.js
+++ webwhatsapi/js/wapi.js
@@ -765,7 +765,13 @@ window.WAPI.sendMessageToID = function (id, message, done) {
 
     firstChat = Store.Chat.models[0];
     var originalID = firstChat.id;
-    firstChat.id = id;
+    if (typeof originalID === 'string') {
+        firstChat.id = id;
+      } else {
+        originalID.server = 'c.us'
+        originalID._serialized = id
+        originalID.user = id.split('@')[0]
+      }
     if (done !== undefined) {
         firstChat.sendMessage(message).then(function () {
             firstChat.id = originalID;
